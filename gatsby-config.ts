58d21750b53495f24821641f675a27a4f9d3a08e
lake.zero <lake.zero@kakaostyle.com>
2023-12-18 12:40:24 +0900

    fix: fix typo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
     siteTitle: `Lake's Blog`,
     siteTitleAlt: `Lake's Blog`,
     siteHeadline: `Lake's Blog`,
-    siteUrl: `https://https://lakezero.netlify.app`,
+    siteUrl: `https://lakezero.netlify.app`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `ko`,
