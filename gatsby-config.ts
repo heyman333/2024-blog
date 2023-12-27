c980ba469531507795d3e9bab240e2fcf6f721a4
lake.zero <lake.zero@kakaostyle.com>
2023-12-27 16:44:28 +0900

    feat: add gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,6 +20,13 @@ const config: GatsbyConfig = {
   trailingSlash: `always`,
   plugins: [
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `G-29WYL4MMJN`,
+        head: true,
+      },
+    },
+    {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
@@ -54,9 +61,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        name: `Lake's Blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `Exploring and Reflecting: A Lake's Tech Blog`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
